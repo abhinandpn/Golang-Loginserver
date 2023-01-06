b517c5af99997f718009388684ba5a81466ccdc5
abhinandpn <55293294+abhinandpn@users.noreply.github.com>
2023-01-06 09:54:53 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,17 @@
+module web
+
+go 1.19
+
+require github.com/gorilla/sessions v1.2.1
+
+require (
+	github.com/alessio/shellescape v1.4.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/tawesoft/golib/v2 v2.6.1 // indirect
+	golang.org/x/exp v0.0.0-20220827204233-334a2380cb91 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
+
+// golang.org/x/sys  v0.1.0
+// require tawesoft.co.uk/go/dialog v0.0.0-20201103210221-4175697d086f
---
+++ main.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/sessions"
+	"github.com/tawesoft/golib/v2/dialog"
+)
+
+// var Check Page
+
+var tpl *template.Template
+
+// session storing
+var Store = sessions.NewCookieStore([]byte("session"))
+
+// template init
+func init() {
+	tpl = template.Must(template.ParseGlob("static/*.html"))
+}
+
+type Page struct {
+	Status bool
+	//user input store the null interface
+	Header1    interface{}
+	IsLoggedin bool
+	Valid      bool
+}
+
+var P = Page{
+	Status: false,
+}
+
+// user data
+var userData = map[string]string{
+	"username": "abhinand",
+	"password": "123",
+}
+
+// login function
+func login(w http.ResponseWriter, r *http.Request) {
+	//clearing
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	//checking the middle ware if user logined or not
+	ok := Middleware(w, r)
+
+	if ok {
+		//redirecting
+		http.Redirect(w, r, "/login-submit", http.StatusSeeOther)
+		return
+	}
+	//if not logined
+	P.Valid = Middleware(w, r)
+	filename := "login.html"
+	err := tpl.ExecuteTemplate(w, filename, P) //redirecting to login page
+	if err != nil {
+		fmt.Println("error while parsing file", err)
+		return
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	//clearing
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+
+	if r.Method == "GET" {
+		//clearing
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		//checking middleware
+		ok := Middleware(w, r)
+		if ok {
+			http.Redirect(w, r, "/", http.StatusSeeOther) //root file redirect
+			return
+		}
+	}
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "there is an error parsing %v", err)
+		return
+	}
+	//store the username and pass
+	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+	//print the username and password
+
+	fmt.Println("username : ", username)
+	fmt.Println("password : ", password)
+	if userData["username"] == username && userData["password"] == password && r.Method == "POST" {
+
+		//session storing
+		session, _ := Store.Get(r, "started")
+
+		//storing the value
+
+		session.Values["username"] = username
+
+		//value moving to P.Hedder1
+		P.Header1 = session.Values["username"]
+
+		//print the value
+		fmt.Println("Hedder 1 value : ", P.Header1)
+
+		//session savingf
+		session.Save(r, w)
+
+		//print the session value
+		fmt.Println("session value : ", session)
+
+		//clearing
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+
+	} else {
+		//wrong allert
+		dialog.Alert("wrong passwod")
+		//if its wrong --
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+
+		return
+
+	}
+
+}
+
+// logout function
+func Logouthandler(w http.ResponseWriter, r *http.Request) {
+	//clearing
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	//session checking if its true
+	if P.Status == true {
+		session, _ := Store.Get(r, "started") //store
+		session.Options.MaxAge = -1           //session time decresing
+		session.Save(r, w)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		P.Status = false //false returing
+		//if its false
+	} else if P.Status == false {
+		http.Redirect(w, r, "/login", http.StatusSeeOther) //redirecting
+	}
+}
+
+// session checking func
+func Middleware(w http.ResponseWriter, r *http.Request) bool { //bool value returning
+	session, _ := Store.Get(r, "started")  //session name
+	if session.Values["username"] == nil { //session value check
+		return false //returning value
+	}
+	P.Header1 = session.Values["username"] //session value
+	return true
+
+}
+
+// index func
+func index(w http.ResponseWriter, r *http.Request) {
+	ok := Middleware(w, r) //middleware check
+	if ok {
+		P.Status = true
+	}
+	filenamE := "index.html" //redirecting file name
+	err := tpl.ExecuteTemplate(w, filenamE, P)
+	if err != nil {
+		fmt.Println("error while parsing file", err) //error
+		return
+	}
+
+}
+
+var Username string
+
+// main
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/login-submit", loginHandler)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", Logouthandler)
+	fmt.Println("server starts at localhost:8080")
+	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
