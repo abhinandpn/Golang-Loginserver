bbd3637cbafae50c882d04a0fc5035654e69934d
abhinandpn <55293294+abhinandpn@users.noreply.github.com>
2023-03-30 10:31:08 +0530

    Updaion
    Add Main function and MiddleWare

--- main.go
+++ main.go
@@ -17,22 +17,18 @@ var tpl *template.Template
 // session storing
 var Store = sessions.NewCookieStore([]byte("session"))
 
-// setting max age of the session
-Store.Options = &sessions.Options{
-    MaxAge: 10, // 10 seconds
-}
-
 // template init
 func init() {
 	tpl = template.Must(template.ParseGlob("static/*.html"))
 }
 
+// var Username string
+
 type Page struct {
 	Status bool
 	//user input store the null interface
-	Header1    interface{}
-	IsLoggedin bool
-	Valid      bool
+	Header1 interface{}
+	Valid   bool
 }
 
 var P = Page{
@@ -45,7 +41,6 @@ var userData = map[string]string{
 	"password": "123",
 }
 
-// login function
 func login(w http.ResponseWriter, r *http.Request) {
 	//clearing
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
@@ -77,7 +72,8 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		//checking middleware
 		ok := Middleware(w, r)
 		if ok {
-			http.Redirect(w, r, "/", http.StatusSeeOther) //root file redirect
+
+			http.Redirect(w, r, "/", http.StatusSeeOther) //route file redirect
 			return
 		}
 	}
@@ -85,7 +81,9 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "there is an error parsing %v", err)
 		return
+
 	}
+
 	//store the username and pass
 	username := r.PostForm.Get("username")
 	password := r.PostForm.Get("password")
@@ -93,22 +91,21 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("username : ", username)
 	fmt.Println("password : ", password)
-		if userData["username"] == username && userData["password"] == password && r.Method == "POST" {
+	if userData["username"] == username && userData["password"] == password && r.Method == "POST" {
 
 		//session storing
 		session, _ := Store.Get(r, "started")
-		// setting max age of the session on per session basis
-		session.Options.MaxAge = 10 // 10 seconds
 		//storing the value
-		session.Values["username"] = username
 
+		session.Values["username"] = username
+		//session age in second
+		session.Options.MaxAge = 60
 		//value moving to P.Hedder1
 		P.Header1 = session.Values["username"]
-
 		//print the value
 		fmt.Println("Hedder 1 value : ", P.Header1)
 
-		//session saving
+		//session savingf
 		session.Save(r, w)
 
 		//print the session value
@@ -135,8 +132,64 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 func Logouthandler(w http.ResponseWriter, r *http.Request) {
 	//clearing
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-	session, _ := Store.Get(r, "started")
-	session.Options.MaxAge = -1 // delete session
-	session.Save(r, w)
-	http.Redirect(w, r, "/login", http.StatusSeeOther)
+	//session checking if its true
+	if P.Status {
+		session, _ := Store.Get(r, "started") //store
+		session.Options.MaxAge = -1           //session time decresing
+		session.Save(r, w)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		P.Status = false //false returing
+		//if its false
+	} else if !P.Status {
+		http.Redirect(w, r, "/login", http.StatusSeeOther) //redirecting
+	}
+}
+
+// session checking func user is logind or not
+func Middleware(w http.ResponseWriter, r *http.Request) bool { //bool value returning
+
+	session, _ := Store.Get(r, "started")  //session name
+	if session.Values["username"] == nil { //session value check
+
+		return false //returning value
+	}
+	P.Header1 = session.Values["username"] //session value
+
+	return true
+
+}
+
+// index func
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+
+	ok := Middleware(w, r) //middleware check
+
+	if ok {
+		P.Status = true
+	} else {
+		P.Status = false
+	}
+
+	filenamE := "index.html" //redirecting file name
+	err := tpl.ExecuteTemplate(w, filenamE, P)
+	if err != nil {
+		fmt.Println("error while parsing file", err) //error
+		return
+	}
+
+}
+
+// main
+func main() {
+	fmt.Println("server starts at localhost:8080")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/login-submit", loginHandler)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", Logouthandler)
+	http.ListenAndServe("localhost:8080", nil)
+	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
